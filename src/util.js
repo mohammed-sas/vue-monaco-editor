4f726f7edab54654e53cc9113430d6f8d95dce49
mohammed-sas <89216938+mohammed-sas@users.noreply.github.com>
2022-07-20 15:28:16 +0530

    fix:removed Module in namspace suggestion

--- src/util.js
+++ src/util.js
@@ -2,11 +2,15 @@ import * as monaco from "monaco-editor";
 import { namespace } from "./constants/nameSpace";
 import { nonTriggeredSuggestions } from "./constants/suggestions";
 export const generateNamespaceSuggestion = (arr) =>
-  arr.map((value) => ({
-    label: value.type,
-    kind: monaco.languages.CompletionItemKind.Field,
-    insertText: `"${value.type}"`,
-  }));
+  arr.map((value) => {
+    if (value.type !== "Module") {
+      return {
+        label: value.type,
+        kind: monaco.languages.CompletionItemKind.Field,
+        insertText: `"${value.type}"`,
+      };
+    }
+  });
 
 const generateSnippet = (functionName, parameters) => {
   let paramString = "";
